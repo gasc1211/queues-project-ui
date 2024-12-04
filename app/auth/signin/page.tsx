5de6160ac2ae4a1423ddb6ca19dcb987cce31c31
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-12-04 12:50:09 -0600

    Add signin submit

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -14,48 +14,67 @@ import {
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { userSignIn } from "@/app/api/handler";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-const UserLoginSchema = z.object({
+const UserSignInSchema = z.object({
   email: z.string(),
   password: z.string()
 })
 
 export default function Signin() {
 
-  const form = useForm<z.infer<typeof UserLoginSchema>>({
-    resolver: zodResolver(UserLoginSchema),
+  const form = useForm<z.infer<typeof UserSignInSchema>>({
+    resolver: zodResolver(UserSignInSchema),
     defaultValues: {
       email: "",
       password: ""
     }
   });
 
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const onSubmit = async (values: z.infer<typeof UserSignInSchema>) => {
+    try {
+      await userSignIn(values);
+      router.push(`/home`);
+    } catch (err) {
+      const error = err as { message: string };
+      setError(error.message);
+    }
+  }
+
   return (
     <main className="h-dvh flex items-center justify-evenly">
       <Card className="[&>*]:mt-4 w-1/3 px-8 py-4">
         <h2 className="font-bold text-center text-2xl">Queues Project</h2>
         <Form {...form}>
-          <FormField control={form.control} name="email" render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <Input placeholder="user@example.com" type="email" {...field} />
-              <FormDescription>Enter your account email</FormDescription>
-            </FormItem>
-          )}
-          />
-          <FormField control={form.control} name="password" render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <Input placeholder="********" type="password" {...field} />
-              <FormDescription>Enter your account password</FormDescription>
-            </FormItem>
-          )}
-          />
-          <div className="flex flex-col align-middle">
-            <Button type="submit">Sign In</Button>
-          </div>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField control={form.control} name="email" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <Input placeholder="user@example.com" type="email" {...field} />
+                <FormDescription>Enter your account email</FormDescription>
+              </FormItem>
+            )}
+            />
+            <FormField control={form.control} name="password" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <Input placeholder="********" type="password" {...field} />
+                <FormDescription>Enter your account password</FormDescription>
+              </FormItem>
+            )}
+            />
+            {error && <p>{error}</p>}
+            <div className="flex flex-col align-middle">
+              <Button type="submit">Sign In</Button>
+            </div>
+          </form>
         </Form>
-        <p className="text-sm text-center">Don't have an account yet?
+        <p className="text-sm text-center">Don&apos;t have an account yet?
           <Button variant="outline" className="ml-2">
             <Link href="/auth/signup">Sign up</Link>
           </Button>
